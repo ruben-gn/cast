873e4d0ac7c8d05a011c231eea8e1efc0fd469ff
Ruben Groot Nibbelink <ruben@grootnibbel.ink>
2026-05-16 21:59:46 +0200

    remove Android module from root build
    The android subproject is developed independently; removing it from
    the root build eliminates Android-specific plugins and deps that have
    no bearing on the server or webapp.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.hilt) apply false
 }
 
 allprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "Cast"
 include("shared-models")
 include("core")
-// include("android")
 
 dependencyResolutionManagement {
     repositories {
