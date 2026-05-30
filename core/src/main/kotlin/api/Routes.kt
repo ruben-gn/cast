0cab8c888b20baa4602c59de3307d3b8eb64d13b
Ruben Groot Nibbelink <ruben@grootnibbel.ink>
2026-05-30 20:57:38 +0200

    api: add HEAD /health endpoint

--- core/src/main/kotlin/api/Routes.kt
+++ core/src/main/kotlin/api/Routes.kt
@@ -1,11 +1,14 @@
 package api
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.di.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.installRoutes() {
     routing {
+        head("/health") { call.respond(HttpStatusCode.OK) }
         route("/api") {
             route("/podcasts") { podcastApi(dependencies) }
             route("/playback") { playbackApi(dependencies) }
